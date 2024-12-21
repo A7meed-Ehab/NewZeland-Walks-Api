56cf33a6244e9d6054aeea1e72e19b4ce8751c10
Ahmed Ehab <a7med_ehab@outlook.com>
2024-12-21 16:53:54 +0200

    seed

---
+++ NZWalks.Api/Controllers/WalksController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.Api.Models.DTO;
+
+namespace NZWalks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public WalksController(IMapper mapper)
+        {
+            this._mapper = mapper;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateWalksDto createWalksDto)
+        {
+            var mappedModel=
+        }
+    }
+}
